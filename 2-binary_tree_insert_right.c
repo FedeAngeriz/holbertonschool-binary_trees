595f17ea209d53274a53f7c18ae6323b3da9abcd
nyacho04 <dnachod83@gmail.com>
2024-12-10 19:19:11 -0300

    `Update binary_tree_insert_right.c: fix memory allocation check and node insertion logic`

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
-* binary_tree_insert_right - Agregar nuevo nodo derecho al árbol binario
-* @parent: puntero a nodo padre
+* binary_tree_insert_right - agregar nuevo nodo derecho al árbol binario
+* @parent: puntero al nodo padre
 * @value: valor a ser guardado en el nuevo nodo
 * Return: nuevo nodo derecho en el árbol binario
 */
@@ -10,10 +10,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNodo = malloc(sizeof(binary_tree_t));
 
-	if (newNodo == NULL)
-		return (NULL);
-
-	if (parent == NULL)
+	if (newNodo == NULL || parent == NULL)
 		return (NULL);
 
 	newNodo->n = value;
@@ -23,27 +20,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent->right != NULL)
 	{
-		newNodo->right = parent->right;
-		parent->right->parent = newNodo;
+		binary_tree_t *old_right = parent->right;
+
+
+		newNodo->right = old_right;
+		old_right->parent = newNodo;
 	}
 
 	parent->right = newNodo;
 
 	return (newNodo);
-}
-
-/**
-* binary_tree_delete - Borrar todo el árbol binario
-* @tree: puntero al nodo raíz
-* Return: nada
-*/
-void binary_tree_delete(binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return;
-
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->right);
-
-	free(tree);
 }
