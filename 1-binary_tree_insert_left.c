0f76109eaa904217885eb4b42e30c3e7047a8c45
nyacho04 <dnachod83@gmail.com>
2024-12-10 18:59:52 -0300

    `Refactor binary tree insertion and deletion functions`

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,32 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - agregar un nuevo nodo izquierdo al arbol binario
- * @parent: puntero al nodo padre
- * @value: variable de datos guardados en el nuevo nodo
- * Return: nuevo nodo izquierdo al arbol binario
- */
+* binary_tree_insert_left - agregar un nuevo nodo izquierdo al arbol binario
+* @parent: puntero al nodo padre
+* @value: variable de datos guardados en el nuevo nodo
+* Return: nuevo nodo izquierdo al arbol binario
+*/
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNodo = malloc(sizeof(binary_tree_t));
-
-	if (newNodo == NULL)
-		return (NULL);
+	binary_tree_t *newNodo;
 
 	if (parent == NULL)
 		return (NULL);
 
+	newNodo = malloc(sizeof(binary_tree_t));
+	if (newNodo == NULL)
+		return (NULL);
+
 	newNodo->n = value;
 	newNodo->parent = parent;
-	newNodo->left = NULL;
+	newNodo->left = parent->left;
 	newNodo->right = NULL;
 
 	if (parent->left != NULL)
-	{
-		newNodo->left = parent->left;
 		parent->left->parent = newNodo;
-	}
+
 	parent->left = newNodo;
+
 	return (newNodo);
 }
