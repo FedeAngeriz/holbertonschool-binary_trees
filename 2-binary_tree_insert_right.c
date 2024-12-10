1be1d1ec632e53f953d063ef539ad7e63ed7a587
nyacho04 <dnachod83@gmail.com>
2024-12-10 19:12:04 -0300

    `Update binary_tree_insert_right.c and add binary_tree_delete function`  to `binary_tree_insert_right.c` and the addition of a new function `binary_tree_delete`.

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
-
 /**
-* binary_tree_insert_right - agregar nuevo nodo derecho al arbol binario
+* binary_tree_insert_right - Agregar nuevo nodo derecho al árbol binario
 * @parent: puntero a nodo padre
-* @value: variable de datos guardados en el nuevo nodo
-* Return: nuevo nodo derecho en el arbol binario
+* @value: valor a ser guardado en el nuevo nodo
+* Return: nuevo nodo derecho en el árbol binario
 */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
@@ -22,7 +21,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	newNodo->left = NULL;
 	newNodo->right = NULL;
 
-
 	if (parent->right != NULL)
 	{
 		newNodo->right = parent->right;
@@ -32,4 +30,20 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	parent->right = newNodo;
 
 	return (newNodo);
+}
+
+/**
+* binary_tree_delete - Borrar todo el árbol binario
+* @tree: puntero al nodo raíz
+* Return: nada
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
 }
