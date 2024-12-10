f034fec6b22a10702536ea3038a184add585c3c1
FedeAngeriz <10086@holbertonstudents.com>
2024-12-10 06:20:02 +0000

    Update 3 is binary trees

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,10 +1,20 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - borrar todo el arbol binario
+ * @tree: puntero al nodo raiz
+ * Return: arbol eliminado
+ */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
 
 	if (tree != NULL)
-		
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+	}
+	free(tree);
 }
