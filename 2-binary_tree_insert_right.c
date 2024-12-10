3221972629b34d7c4fe97a7d10a63eaf1905ff61
nyacho04 <dnachod83@gmail.com>
2024-12-10 19:22:24 -0300

    `Fixed binary_tree_insert_right function to correctly handle edge cases and node assignment`

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,23 +8,30 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNodo = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNodo;
 
-	if (newNodo == NULL || parent == NULL)
+	if (parent == NULL)
+		return (NULL);
+
+	newNodo = malloc(sizeof(binary_tree_t));
+
+	if (newNodo == NULL)
 		return (NULL);
 
 	newNodo->n = value;
 	newNodo->parent = parent;
 	newNodo->left = NULL;
-	newNodo->right = NULL;
 
 	if (parent->right != NULL)
 	{
 		binary_tree_t *old_right = parent->right;
 
-
 		newNodo->right = old_right;
 		old_right->parent = newNodo;
+	}
+	else
+	{
+		newNodo->right = NULL;
 	}
 
 	parent->right = newNodo;
