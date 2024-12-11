68d7861198f76ef82094dae6aabe93d79b827138
nyacho04 <dnachod83@gmail.com>
2024-12-10 21:30:26 -0300

    task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ *  binary_tree_postorder - prints a binary tree in postorder
+ *
+ * @tree: the root of the binary tree
+ *
+ * @func: the function to call on each node
+ *
+ * Return: void
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
