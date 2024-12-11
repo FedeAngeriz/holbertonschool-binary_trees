1a14d20d9750f1d5d22c19c2231c8cba52dbd316
nyacho04 <dnachod83@gmail.com>
2024-12-11 11:33:39 -0300

    refactor tak 14 binary_tree_height function to correctly calculate height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - Measures the height of a binary tree
- *
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: The height of the tree. If tree is NULL, return 0
- */
+* binary_tree_height - Measures the height of a binary tree
+*
+* @tree: Pointer to the root node of the tree to measure the height
+*
+* Return: The height of the tree. If tree is NULL, return 0
+*/
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -16,10 +16,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
 
-	return (1 + (left > right ? left : right));
+	left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+
+	return (left > right ? left : right);
 }
 /**
 * binary_tree_balance - Measures the balance factor of a binary tree
@@ -32,6 +33,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height;
+
 	int right_height;
 
 	if (!tree)
